5c5a0254f76a38ab9329a8f416c829982ef37404
khole_taro <2965939109@qq.com>
2021-03-07 11:38:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+这个是我博客的后端，采用springboot来写的。
+我之前用ssm写了一个[博客](https://github.com/circle111/khole_blog)。
+现在又要重构，如果只是照搬也太没有意思了。
+
+这次会用到的技术栈是：springboot、mybatis、mysql、druid、swagger、shiro
+
+依旧采用前后端分离的方式搭建后端接口。
---
+++ src/main/java/com/circle/KholeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.circle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KholeSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KholeSpringbootApplication.class, args);
+    }
+
+}
